a230e8463263950838ffbfa28fa2ae4c1647cf68
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-25 11:13:35 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+```python
+from setuptools import setup, find_packages
+
+setup(
+    name='chain-of-thought',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'logging',
+    ],
+    description='Chain-of-Thought Reasoning Framework',
+)
